3ce207a83ca420818db3459425a1024d9f094810
Dylan <dylanredfield0938@gmail.com>
2018-11-06 19:29:29 +0000

    Basic stories

---
+++ Textual/stories.md
@@ -0,0 +1,9 @@
+## check open
+* check_is_open
+    - action_check_is_open
+
+## Check is open on day
+* check_is_open{"time": "2018-xxxxxx"}
+    - action_check_is_open_on_day
+
+
--- Textual/train_online.py
+++ Textual/train_online.py
@@ -1,10 +1,11 @@
 from rasa_core.train import online
+from rasa_core.interpreter import RasaNLUInterpreter
 from rasa_nlu.model import Interpreter
 from rasa_core.utils import EndpointConfig
 from rasa_core import utils
 from rasa_core.agent import Agent
 
-interpreter = Interpreter.load("./models/current/nlu")
+interpreter = RasaNLUInterpreter("models/current/nlu")
 endpoint_conf = utils.read_endpoint_config("endpoints.yml", endpoint_type="action_endpoint")
 agent = Agent.load("models/dialogue", interpreter = interpreter, action_endpoint = endpoint_conf)
 online.serve_agent(agent)
