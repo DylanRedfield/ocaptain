3621b79474fab8a355397ccba1f7955d85c3eb52
Dylan <dylanredfield0938@gmail.com>
2019-01-22 06:33:07 +0000

    ugh

--- textual/stories/reservation_temp_times_filling.md
+++
@@ -1,7 +0,0 @@
-## checkpoint_to_fill_temp_times
-> checkpoint_to_fill_temp_times
-* inform{"number": 2}
-    - action_set_potential_size_slot
-    - slot{"potential_size": 1}
-> checkpoint_with_potential_size_to_validate_and_fill_size
-
--- textual/train_online.sh
+++ textual/train_online.sh
@@ -1,1 +1,1 @@
-python3 -m rasa_core.train interactive -o models/dialogue -d domain.yml -s stories --nlu models/current/nlu --endpoints endpoints.yml --skip_visualization
+python3 -m rasa_core.train interactive --skip_visualization -o models/dialogue -d domain.yml -s stories --nlu models/current/nlu --endpoints endpoints.yml 
