b3b13ae59c678f65a058be56c14ec65dfa5faa7f
dylan <dylanredfieldsoftware@gmail.com>
2019-04-21 15:41:47 -0400

    Fixed opentable base url

--- PizzaBot/opentable.go
+++ PizzaBot/opentable.go
@@ -31,11 +31,9 @@ func Query(id string, datetime time.Time, partySize string) (OpenTableResult, er
 	searchHour := datetime.Hour()
 	searchMinute := datetime.Minute()
 
-	baseUrl := "https://www.opentable.com/restaurant/profile/"
-	paramUrl := fmt.Sprintf("%s%s/reserve?restref=%s&datetime=%d-%d-%dT%d:%d&covers=%s",
+	baseUrl := "https://www.opentable.com/restref/client/"
+	paramUrl := fmt.Sprintf("%s?rid=%s&restref=%s&datetime=%d-%02d-%02dT%02d:%02d&partysize=%s",
 		baseUrl, id, id, searchYear, searchMonth, searchDay, searchHour, searchMinute, partySize)
-
-    log.Println(paramUrl)
 	res, err := http.Get(paramUrl)
 
 	if err != nil {
